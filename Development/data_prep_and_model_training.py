8115d86dc59cf991cd7830fe9b2fe48c4cfe1d26
Fabio <Fabio.Greenwood@gmail.com>
2023-10-02 17:53:50 +0100

    transfered changes to data prep and model training file, now I must redesign the DoE

--- Development/data_prep_and_model_training.py
+++ Development/data_prep_and_model_training.py
@@ -243,6 +243,22 @@ def return_annotated_tweets_name(company_symbol, train_period_start, train_perio
 def return_ticker_code_1(filename):
     return filename[:filename.index(".")]
 
+def fg_timer(curr_iter_num, total_iter_num, callback_counts, task_name="", start_time=None):
+    if ((curr_iter_num) % int(total_iter_num / callback_counts) == 0 and curr_iter_num > 0) or curr_iter_num == total_iter_num - 1:
+        output_string = ""
+        if not task_name=="":
+            output_string += task_name + " : "
+        output_string += str(curr_iter_num) + "/" + str(total_iter_num) + " "
+        output_string += datetime.now().strftime("%H:%M:%S")
+        if not start_time==None:
+            time_past = datetime.now() - start_time
+            output_string += " : etc: " + (start_time + (time_past / (curr_iter_num / total_iter_num))).strftime("%H:%M:%S")
+        print(output_string)
+        
+    return curr_iter_num + 1
+
+
+
 #%%SubModule – Stock Market Data Prep 
 
 def import_financial_data(
@@ -447,7 +463,7 @@ def retrieve_sentimental_data():
     raise ValueError('needs writing') 
     return None
 
-def generate_sentimental_data(index, temporal_params_dict, fin_inputs_params_dict, senti_inputs_params_dict, outputs_params_dict, model_hyper_params, training_or_testing="training"):
+def generate_sentimental_data(index, temporal_params_dict, fin_inputs_params_dict, senti_inputs_params_dict, outputs_params_dict, model_hyper_params, repeat_timer=10, training_or_testing="training", hardcode_df_annotated_tweets=None):
     
     #general parameters
     global global_financial_history_folder_path, global_precalculated_assets_locations_dict
@@ -461,6 +477,7 @@ def generate_sentimental_data(index, temporal_params_dict, fin_inputs_params_dic
     relavance_halflife      = senti_inputs_params_dict["relative_halflife"]
     weighted_topics         = senti_inputs_params_dict["weighted_topics"]
     apply_IDF               = senti_inputs_params_dict["apply_IDF"]
+    start_time              = datetime.now()
 
     if training_or_testing == "training" or training_or_testing == "train":
         train_period_start  = temporal_params_dict["train_period_start"]
@@ -472,20 +489,23 @@ def generate_sentimental_data(index, temporal_params_dict, fin_inputs_params_dic
         global global_error_str_1
         raise ValueError(global_error_str_1.format(str(training_or_testing)))
     
-    
     #search for annotated tweets
     annotated_tweets_folder_location_string = global_precalculated_assets_locations_dict["root"] + global_precalculated_assets_locations_dict["annotated_tweets"]
     annotated_tweets_name = return_annotated_tweets_name(company_symbol, train_period_start, train_period_end, weighted_topics, num_topics, topic_model_alpha, apply_IDF, tweet_ratio_removed)
     annotated_tweets_location_file = annotated_tweets_folder_location_string + annotated_tweets_name + ".csv"
-    if os.path.exists(annotated_tweets_location_file):
+    print("approaching gate")
+    if isinstance(hardcode_df_annotated_tweets, pd.DataFrame):
+        print("gate works")
+        df_annotated_tweets = hardcode_df_annotated_tweets
+        print(datetime.now().strftime("%H:%M:%S") + " - generating sentimental data")
+    elif os.path.exists(annotated_tweets_location_file):
         df_annotated_tweets = pd.read_csv(annotated_tweets_location_file)
         df_annotated_tweets.set_index(df_annotated_tweets.columns[0], inplace=True)
         df_annotated_tweets.index.name = "datetime"
     else:
         df_annotated_tweets = generate_annotated_tweets(temporal_params_dict, fin_inputs_params_dict, senti_inputs_params_dict, outputs_params_dict, model_hyper_params)
         df_annotated_tweets.to_csv(annotated_tweets_location_file)
-    
-    print(datetime.now().strftime("%H:%M:%S") + " - generating sentimental data")
+        print(datetime.now().strftime("%H:%M:%S") + " - generating sentimental data")
     
     #generate sentimental data from topic model and annotate tweets
     #index = generate_datetimes(train_period_start, train_period_end, seconds_per_time_steps)
@@ -503,8 +523,16 @@ def generate_sentimental_data(index, temporal_params_dict, fin_inputs_params_dic
     tweet_cohort_end    = tweet_cohort_end.total_seconds()
     tweet_cohort        = pd.DataFrame(columns=df_annotated_tweets.columns)
     tweet_cohort, df_annotated_tweets  = update_tweet_cohort(tweet_cohort, df_annotated_tweets, tweet_cohort_start, tweet_cohort_end)
+    index_list = list(index)
+    print(type(index_list))
+    rep_num = int(len(index_list) / 10)
     
+    count = 0; counter_len = len(index) # FG_Counter
     for time_step in index:
+        #if index_list.index(time_step) % rep_num == 0:
+        #    print(str(index_list.index(time_step)/ (rep_num * 10)))
+        #    print(datetime.now().strftime("%H:%M:%S"))
+            
         senti_scores = list(np.zeros(num_topics))
         tweet_cohort["post_date"] = tweet_cohort["post_date"].astype(float)
         tweet_cohort["~sent_overall"] = tweet_cohort["~sent_overall"].astype(float)
@@ -520,10 +548,14 @@ def generate_sentimental_data(index, temporal_params_dict, fin_inputs_params_dic
         tweet_cohort_start += seconds_per_time_steps
         tweet_cohort_end   += seconds_per_time_steps
         tweet_cohort, df_annotated_tweets = update_tweet_cohort(tweet_cohort, df_annotated_tweets, tweet_cohort_start, tweet_cohort_end)
+        #timer
+        if repeat_timer==True:
+            count = fg_timer(count, counter_len, repeat_timer, task_name="sentimental_tweets", start_time=start_time)
+            
     
     return df_sentiment_scores
 
-def generate_annotated_tweets(temporal_params_dict, fin_inputs_params_dict, senti_inputs_params_dict, outputs_params_dict, model_hyper_params, training_or_testing="training"):
+def generate_annotated_tweets(temporal_params_dict, fin_inputs_params_dict, senti_inputs_params_dict, outputs_params_dict, model_hyper_params, repeat_timer=10, training_or_testing="training", hardcode_location_for_topic_model=""):
     #general parameters
     global global_financial_history_folder_path, global_precalculated_assets_locations_dict
     company_symbol      = outputs_params_dict["output_symbol_indicators_tuple"][0]
@@ -535,6 +567,7 @@ def generate_annotated_tweets(temporal_params_dict, fin_inputs_params_dict, sent
     relavance_lifetime  = senti_inputs_params_dict["relative_lifetime"]
     apply_IDF           = senti_inputs_params_dict["apply_IDF"]
     sentiment_method    = senti_inputs_params_dict["sentiment_method"]
+    start_time          = datetime.now()
     
     if training_or_testing == "training" or training_or_testing == "train":
         train_period_start  = temporal_params_dict["train_period_start"]
@@ -553,11 +586,15 @@ def generate_annotated_tweets(temporal_params_dict, fin_inputs_params_dict, sent
     
     topic_model_name = return_topic_model_name(num_topics, topic_model_alpha, apply_IDF, tweet_ratio_removed)
     topic_model_location_file = topic_model_folder_folder + topic_model_name
-    if os.path.exists(topic_model_location_file + "topic_model_dict_" + topic_model_name + ".pkl"):
+    if not hardcode_location_for_topic_model=="":
+        with open(hardcode_location_for_topic_model, 'rb') as file:
+            topic_model_dict = pickle.load(file)
+    elif os.path.exists(topic_model_location_file + "topic_model_dict_" + topic_model_name + ".pkl"):
         with open(topic_model_location_file + "topic_model_dict_" + topic_model_name + ".pkl", "rb") as file:
             topic_model_dict = pickle.load(file)
     else:
         wordcloud, topic_model_dict, visualisation = generate_and_save_topic_model(topic_model_name, temporal_params_dict, fin_inputs_params_dict, senti_inputs_params_dict, outputs_params_dict, model_hyper_params)
+  
     
     #generate annotated tweets
     print(datetime.now().strftime("%H:%M:%S") + " - generating annotated tweets")
@@ -567,7 +604,7 @@ def generate_annotated_tweets(temporal_params_dict, fin_inputs_params_dict, sent
         columns_to_add = columns_to_add + ["~sent_topic_W" + str(num)]
     
     df_annotated_tweets[columns_to_add] = float("nan")
-    count = 0 # FG_Counter
+    count = 0; counter_len = len(df_annotated_tweets.index) # FG_Counter
     
     for tweet_id in df_annotated_tweets.index:
         
@@ -582,6 +619,9 @@ def generate_annotated_tweets(temporal_params_dict, fin_inputs_params_dict, sent
                 topic_weights[tup[0]] = tup[1]
         sentiment_analysis = [sentiment_value] + topic_weights
         df_annotated_tweets.loc[tweet_id, columns_to_add] = sentiment_analysis
+        #timer
+        if repeat_timer>0:
+            count = fg_timer(count, counter_len, repeat_timer, task_name="annotated_data", start_time=start_time)
     
     return df_annotated_tweets
 
@@ -653,6 +693,17 @@ def import_twitter_data_period(target_file, period_start, period_end, relavance_
     
     return input_df
 
+from datetime import datetime, timedelta
+def convert_epoch_time_to_datetime_string(post_date):
+    period = datetime(1970, 1, 1) + timedelta(seconds=post_date)
+    return period.strftime('%d/%m/%y %H:%M:%S')
+
+from datetime import datetime, timedelta
+def return_time(post_date):
+    period = datetime(1970, 1, 1) + timedelta(seconds=post_date)
+    return period.strftime('%d/%m/%y %H:%M:%S')
+
+
 def prep_and_save_twitter_text_for_subject_discovery(input_list, df_stocks_list_file=None, df_prepped_tweets_company_agnostic_file_path=None):
     global global_precalculated_assets_locations_dict
     #prep parameters
@@ -692,7 +743,7 @@ def prep_and_save_twitter_text_for_subject_discovery(input_list, df_stocks_list_
                     Removed = True
                     break
             if Removed == False:
-                for char in tickers_list + stopwords_english + corp_stopwords + misc_stopwords:
+                for char in tickers_list + stopwords_english + misc_stopwords + list(reversed(stocks_list)) + list(reversed(stocks_list_shortened)): #corp_stopwords + 
                     if char == word:
                         tweet.remove(word)
                         S = False
@@ -702,13 +753,13 @@ def prep_and_save_twitter_text_for_subject_discovery(input_list, df_stocks_list_
         
     #finalise and remove stock names
     output = []
-    iteration_list = list(reversed(stocks_list)) + list(reversed(stocks_list_shortened))
+    #iteration_list = list(reversed(stocks_list)) + list(reversed(stocks_list_shortened))
     for split_tweet in split_tweets:
         #recombined_tweet = list(map(lambda x: x.strip(), split_tweet))
         recombined_tweet = " ".join(split_tweet)#.replace("  "," ")
         #recombined_tweet = " ".join(recombined_tweet)#.replace("  "," ")
-        for stock_name in iteration_list:
-            recombined_tweet = recombined_tweet.replace(stock_name, "")
+        #for stock_name in iteration_list:
+        #    recombined_tweet = recombined_tweet.replace(stock_name, "")
         output = output + [recombined_tweet]
     
     
@@ -737,9 +788,29 @@ def update_shortened_company_file(df_stocks_list_file, corp_stopwords, file_loca
     
     return stocks_list_shortened_dict
 
+def return_initial_eta(num_topics, initial_topics_dict, id2word):
+    eta = np.ones((num_topics, len(id2word.id2token))) * 0.5
+    not_found_words_list = []
+    for topic_num in range(min(num_topics,len(initial_topics_dict))):
+        for enforced_word_str in initial_topics_dict[topic_num]:
+            if enforced_word_str in id2word.token2id.keys():
+                enforced_word_id = id2word.token2id[enforced_word_str]
+                # enforce word for topic and weaken for others
+                for topic_num_2 in range(num_topics):
+                    if topic_num_2 == topic_num:
+                        eta[topic_num_2][enforced_word_id] = 0.9
+                    else: 
+                        eta[topic_num_2][enforced_word_id] = 0.1
+            else:
+                not_found_words_list = not_found_words_list + [enforced_word_str]
+    print("{} words not found: {}".format(str(len(not_found_words_list)), str(not_found_words_list)))
+    return eta
+
+
+
 def return_subject_keys(df_prepped_tweets_company_agnostic, topic_qty = 10, enforced_topics_dict=None, stock_names_list=None, words_to_remove = None, 
                         return_LDA_model=True, return_png_visualisation=False, return_html_visualisation=False, 
-                        topic_model_alpha=0.1, apply_IDF=True, cores=2):
+                        topic_model_alpha=0.1, apply_IDF=True, cores=2, passes=20, iterations=400):
     output = []
 
     data = df_prepped_tweets_company_agnostic
@@ -770,18 +841,15 @@ def return_subject_keys(df_prepped_tweets_company_agnostic, topic_qty = 10, enfo
         corpus = [id2word.doc2bow(text) for text in texts]
 
         #translate the enforced_topics_dict input
-        eta = None
         if enforced_topics_dict != None:
-            eta = np.zeros(len(id2word))
-            offset = 1
-            for group_num in range(len(enforced_topics_dict)):
-                for word in enforced_topics_dict[group_num]:
-                    try: 
-                        word_id = id2word.token2id[word]
-                        eta[word_id] = group_num + offset
-                    except:
-                        a=1
-
+            if len(id2word.id2token) == 0:
+                #manual transfer to id2token, required.
+                for key in id2word.token2id:
+                    id2word.id2token[id2word.token2id[key]] = key
+            eta = return_initial_eta(topic_qty, enforced_topics_dict, id2word)
+        else:
+            eta='auto' # Let Gensim estimate eta (topic-word distribution)
+        #eta = np.zeros(len(id2word.id2token)) #FG_action: remove
         #apply IDF
         if apply_IDF == True:
             # create tfidf model
@@ -791,19 +859,19 @@ def return_subject_keys(df_prepped_tweets_company_agnostic, topic_qty = 10, enfo
             corpus = tfidf[corpus]
         
         # Build LDA model
-        
         lda_model = gensim.models.LdaModel(corpus=corpus,
         #lda_model = gensim.models.LdaMulticore(corpus=corpus,
-                                               id2word=id2word,
-                                               num_topics=topic_qty,
-                                               eta=eta,
-                                               alpha = topic_model_alpha, # controls topic sparsity
-                                               #beta = beta, # controls word sparsity
-                                               #workers=cores
-                                               )
+                                                id2word=id2word,
+                                                num_topics=topic_qty,
+                                                alpha = topic_model_alpha, # controls topic sparsity:- Set a low alpha to make topics more focused
+                                                eta=eta, # sets the initial topic-word distribution
+                                                passes=passes,  # Number of training iterations
+                                                iterations=iterations,  # Number of iterations per pass
+                                                random_state=42)
         
         # Print the Keyword in the 10 topics
         #pprint(lda_model.print_topics())
+        #eta = lda_model.expElogbeta
         doc_lda = lda_model[corpus]
         topic_model_dict = {"lda_model" : lda_model, "doc_lda" : doc_lda, "corpus" : corpus, "id2word" : id2word}
         output = output + [topic_model_dict]
