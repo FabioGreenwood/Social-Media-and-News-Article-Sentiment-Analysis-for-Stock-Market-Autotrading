2036fedd205a4ae96ce9bf8577cbf3462394ad0a
Nick Greenwood <fabio.greenwood@gmail.com>
2023-12-06 22:26:44 +0000

    about to try predicting multiple batches of generator to a single model

--- Experimental/RNN/RNN second draft.py
+++ Experimental/RNN/RNN second draft.py
@@ -11,15 +11,22 @@ import matplotlib as plt
 import copy
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import os
+from datetime import datetime
 
+# parameters
+master_folder_path = r"C:\Users\Public\fabio_uni_work\Social-Media-and-News-Article-Sentiment-Analysis-for-Stock-Market-Autotrading"
+example_input_path = os.path.join(master_folder_path, "example_input.csv")
+date_format = "%m/%d/%Y %H:%M"
 
+list_of_precalculated_asset_folders = ["always_up_down_results", "annotated_tweets", "cleaned_tweets_ready_for_subject_discovery", "experimental_records", "predictive_model", "sentiment_data", "technical_indicators", "temp_testing_dicts", "topic_models"]
 
 # Example multivariate time series data (replace this with your actual time series)
 # Assuming two features in the time series
 
-
 # Create random multivariate time series data
-df_X = pd.read_csv(r"C:\Users\Fabio\OneDrive\Documents\Studies\Final Project\Social-Media-and-News-Article-Sentiment-Analysis-for-Stock-Market-Autotrading\precalculated_assets\example_input.csv")
+df_X = pd.read_csv(example_input_path)
 df_X.set_index(df_X.columns[0], inplace=True)
 #df_y = pd.read_csv(r"C:\Users\Fabio\OneDrive\Documents\Studies\Final Project\Social-Media-and-News-Article-Sentiment-Analysis-for-Stock-Market-Autotrading\precalculated_assets\example_output.csv")
 #df_y.set_index(df_y.columns[0], inplace=True)
@@ -34,8 +41,12 @@ df_y = df_X["£_close"].shift(-pred_steps)
 df_X = df_X[:-pred_steps]
 df_y = df_y[:-pred_steps]
 
+# Normalize the data
+scaler_X = MinMaxScaler()
+scaler_y = MinMaxScaler()
 
-keras.utils.timeseries_dataset_from_array
+df_X_normalized = scaler_X.fit_transform(df_X)
+df_y_normalized = scaler_y.fit_transform(df_y.values.reshape(-1, 1))
 
 
 sequence_length = 10
@@ -44,30 +55,52 @@ batch_size = 32
 
 
 training_generator = tf.keras.preprocessing.sequence.TimeseriesGenerator(
-        df_X.values,
-        df_y.values,
+        df_X_normalized,
+        df_y_normalized,
         sequence_length,
         batch_size=1,
         shuffle=False
     )
 
 datetime_generator = tf.keras.preprocessing.sequence.TimeseriesGenerator(
-        df_X.index.values,
-        df_y.values,
+        df_X.index,
+        df_y_normalized,
         sequence_length,
         batch_size=1,
         shuffle=False
     )
+
 
 ## remove sequences that straddle a day
 #for id in range(len(training_generator)):
 #    if not datetime_generator[id][0][0][0][:10] == datetime_generator[id][0][0][-1][:10]:
 #        training_generator[id] = None, None
 
+
+def return_filtered_batches_that_dont_cross_two_days(training_generator, datetime_generator):
+    mask = []
+    for batch_x, output in datetime_generator:
+        if datetime.strptime(batch_x[0][0], date_format).day == datetime.strptime(batch_x[0][-1], date_format).day:
+            mask += [True]
+        else:
+            mask += [False]
+    filtered_training_generator_batch_list = []
+    for training_batch, Bool in zip(training_generator, mask):
+        if Bool == True:
+            filtered_training_generator_batch_list += [training_batch]
+    return filtered_training_generator_batch_list
+
+## Filter batches that cross two days
+#filtered_batches = [batch for batch in generator if not batch_crosses_two_days(batch)]
+
+filtered_training_generator_batch_list = return_filtered_batches_that_dont_cross_two_days(training_generator, datetime_generator)
+
+
+
 model = Sequential([
-    LSTM(units=50, activation='linear', return_sequences=True, input_shape=input_shape),
-    LSTM(units=50, activation='linear', return_sequences=True),
-    LSTM(units=50, activation='linear'),
+    LSTM(units=50, activation='relu', return_sequences=True, input_shape=input_shape),
+    LSTM(units=50, activation='relu', return_sequences=True),
+    LSTM(units=50, activation='relu'),
     Dense(units=1, activation='linear')
 ])
 model.compile(optimizer='adam', loss='mae')
@@ -75,15 +108,9 @@ model.fit(training_generator, epochs=5)
 
 #training 
 train_predictions = model.predict(training_generator)
-predicted_values = train_predictions.reshape(-1)[:-pred_steps]
-
-"""# manual training check
-train_predictions_manually_generated_input = df_X.iloc[-10:,:].values.reshape((1, sequence_length, df_X.shape[1]))
-train_predictions_2 = model.predict(train_predictions_manually_generated_input)
-"""
+predicted_values = scaler_y.inverse_transform(train_predictions).reshape(-1)[:-pred_steps]
 
 # "validate" the model
-#validation_dataset = timeseries_dataset_from_array(df_X.values, df_y.values, sequence_length=1, sampling_rate=1, shuffle=False)
 training_score = model.evaluate(training_generator)
 print("Validation Loss:", training_score)
 
@@ -93,11 +120,9 @@ print("Validation Loss:", training_score)
 # Extract the true y values for the chosen sequence
 true_values = list(df_y.iloc[:].values.flatten())[sequence_length+pred_steps:]
 
-
 print("r2 loss:", str(r2_score(true_values, predicted_values)))
 print("mse loss:", str(mean_squared_error(true_values, predicted_values)))
 print("mae loss:", str(mean_absolute_error(true_values, predicted_values)))
-
 
 # Create a time axis for plotting
 time_steps = np.arange(len(predicted_values))
