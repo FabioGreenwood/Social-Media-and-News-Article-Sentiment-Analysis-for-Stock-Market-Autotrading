999f3b800f770de4011bd8a987cc7c2f7223e6f6
Nick Greenwood <fabio.greenwood@gmail.com>
2024-02-10 13:23:17 +0000

    small changes made to reporting

--- Development/additional_reporting_and_model_trading_runs.py
+++ Development/additional_reporting_and_model_trading_runs.py
@@ -52,7 +52,9 @@ def return_results_X_min_plus_minus_accuracy(y_preds_input, y_test_input, pred_s
     # variables and checks
     if financial_value_scaling==None:
         raise ValueError("financial_value_scaling must be set")
-    
+    if len(pred_steps_list) > 1:
+        raise ValueError("expecting only a length of one")
+    steps_back = pred_steps_list[0]
     
     results_dict = {
         "results_bets_with_confidence_proportion" : dict(), 
@@ -66,77 +68,72 @@ def return_results_X_min_plus_minus_accuracy(y_preds_input, y_test_input, pred_s
     
     
     # logic
-    for steps_back in pred_steps_list:
-        #initialise variables
-        results_dict["results_bets_with_confidence_proportion"][steps_back] = dict()
-        results_dict["results_x_mins_PC"][steps_back]                       = dict()
-        results_dict["results_x_mins_score"][steps_back]                    = dict()
-        results_dict["results_x_mins_weighted"][steps_back]                 = dict()
-        
-        y_preds, y_test = copy.deepcopy(y_preds_input), copy.deepcopy(y_test_input)
-        y_original_val  = copy.deepcopy(y_test_input)
-        # this time delta is reversed as now the test(actual), pred(predicted) & original value for a give prediction all share the same index value
-        y_original_val.index += timedelta(seconds=steps_back*seconds_per_time_steps)
+    #steps_back in pred_steps_list
+    #initialise variables
+    results_dict["results_bets_with_confidence_proportion"][steps_back] = dict()
+    results_dict["results_x_mins_PC"][steps_back]                       = dict()
+    results_dict["results_x_mins_score"][steps_back]                    = dict()
+    results_dict["results_x_mins_weighted"][steps_back]                 = dict()
+    
+    y_preds = y_preds_input.copy()
+    y_test = y_test_input.copy()
+    y_original_val  = y_test_input.copy()
+    y_original_val.index += timedelta(seconds=steps_back*seconds_per_time_steps)
 
-        # trim dfs so they all share the same index
-        if isinstance(y_preds, pd.Series):
-            y_preds_inputs = copy.deepcopy(y_preds)
-            y_preds = pd.DataFrame(y_preds)
-        merged_df = pd.merge(y_test, y_preds, left_index=True, right_index=True, how='inner')
-        merged_df = pd.merge(merged_df, y_original_val, left_index=True, right_index=True, how='inner')
-        y_test = y_test.loc[merged_df.index]
-        y_preds = y_preds.loc[merged_df.index]
-        y_original_val = y_original_val.loc[merged_df.index]
+    # trim dfs so they all share the same index
+    if isinstance(y_preds, pd.Series):
+        y_preds = pd.DataFrame(y_preds)
+    merged_df = pd.merge(y_test, y_preds, left_index=True, right_index=True, how='inner')
+    merged_df = pd.merge(merged_df, y_original_val, left_index=True, right_index=True, how='inner')
+    y_test = y_test.loc[merged_df.index]
+    y_preds = y_preds.loc[merged_df.index]
+    y_original_val = y_original_val.loc[merged_df.index]
         
-        #adjust if the data input is not in the "price delta form"
-        if financial_value_scaling != "delta_scaled":
-            df_x_values_delta, df_y_values_delta = pd.DataFrame(columns=["delta_price"]), pd.DataFrame(columns=["delta_price"])
-            test_col_temp, pred_col_temp = y_test.columns[0], y_preds.columns[0]
-            for prediction_index in merged_df.index[steps_back:]:
-                df_x_values_delta.loc[prediction_index, "delta_price"] = y_test.loc[prediction_index,test_col_temp]  - y_original_val.loc[prediction_index,test_col_temp]
-                df_y_values_delta.loc[prediction_index, "delta_price"] = y_preds.loc[prediction_index,pred_col_temp] - y_original_val.loc[prediction_index,test_col_temp]
-        else: # no convertion needed
-            df_x_values_delta = y_test
-            df_y_values_delta = y_preds
-                
-        for confidence_threshold_key in confidences_before_betting_PC:
+    if financial_value_scaling != "delta_scaled":
+        df_x_values_delta = y_test - y_original_val
+        df_y_values_delta = y_preds - y_original_val
+    else:
+        df_x_values_delta = y_test
+        df_y_values_delta = y_preds
             
-            #refresh values
-            x = copy.deepcopy(df_x_values_delta)
-            y = copy.deepcopy(df_y_values_delta)
-            x.columns, y.columns = ["delta_price"], ["delta_price"]
-            # confidence threshold is now taken from the percentile
-            confidence_threshold_adjusted = np.percentile(abs(y),confidence_threshold_key * 100)
+    for confidence_threshold_key in confidences_before_betting_PC:
+        
+        #refresh values
+        x = df_x_values_delta.copy()
+        y = df_y_values_delta.copy()
+        x.columns, y.columns = ["delta_price"], ["delta_price"]
+        # confidence threshold is now taken from the percentile
+        confidence_threshold_adjusted = np.percentile(abs(y),confidence_threshold_key * 100)
 
-            ## proportion of bets taken
-            # filter out bets not confident to make
-            y["delta_price"] = y["delta_price"].apply(lambda y: y if abs(y) > confidence_threshold_adjusted else 0)
-            results_dict["results_bets_with_confidence_proportion"][steps_back][confidence_threshold_key] = max(0,(abs(y["delta_price"]) > 0).sum() / len(y["delta_price"]))
+        ## proportion of bets taken
+        # filter out bets not confident to make
+        y["delta_price"] = y["delta_price"].apply(lambda y: y if abs(y) > confidence_threshold_adjusted else 0)
+        results_dict["results_bets_with_confidence_proportion"][steps_back][confidence_threshold_key] = max(0,(abs(y["delta_price"]) > 0).sum() / len(y["delta_price"]))
 
-            if results_dict["results_bets_with_confidence_proportion"][steps_back][confidence_threshold_key] != 0:
-                ## proportion up/down correctly bet
-                z = x * y
-                results_dict["results_x_mins_PC"][steps_back][confidence_threshold_key] = (z["delta_price"] > 0).sum() / (abs(z["delta_price"]) > 0).sum()
+        if results_dict["results_bets_with_confidence_proportion"][steps_back][confidence_threshold_key] != 0:
+            ## proportion up/down correctly bet
+            z = x * y
+            results_dict["results_x_mins_PC"][steps_back][confidence_threshold_key] = (z["delta_price"] > 0).sum() / (abs(z["delta_price"]) > 0).sum()
 
-                ## first scoring method
-                # give one weighting to all accepted bets
-                stake_a = pd.DataFrame()
-                stake_a["delta_price"] = y["delta_price"].apply(lambda y: y/abs(y) if abs(y) > confidence_threshold_adjusted else 0)
-                score_correct = (stake_a*x).sum()
-                score_both = abs(stake_a*x).sum()
-                results_dict["results_x_mins_score"][steps_back][confidence_threshold_key] = score_correct.values[0] / score_both.values[0]
+            ## first scoring method
+            # give one weighting to all accepted bets
+            stake_a = pd.DataFrame()
+            stake_a["delta_price"] = y["delta_price"].apply(lambda y: y/abs(y) if abs(y) > confidence_threshold_adjusted else 0)
+            score_correct = (stake_a*x).sum()
+            score_both = abs(stake_a*x).sum()
+            results_dict["results_x_mins_score"][steps_back][confidence_threshold_key] = score_correct.values[0] / score_both.values[0]
 
-                ## second scoring method FG_action: replace
-                # calc stake in all accepted bets
-                stake_b = pd.DataFrame()
-                stake_b["delta_price"] = y["delta_price"].apply(lambda y: y - 0.5 * confidence_threshold_adjusted if y > confidence_threshold_adjusted else (y + 0.5 * confidence_threshold_adjusted if -y > confidence_threshold_adjusted else 0))
-                score_correct = (stake_b*x).sum()
-                score_both = abs(stake_b*x).sum()
-                results_dict["results_x_mins_weighted"][steps_back][confidence_threshold_key] = score_correct.values[0] / score_both.values[0]
-            else:
-                results_dict["results_x_mins_PC"][steps_back][confidence_threshold_key]         = 0.0
-                results_dict["results_x_mins_score"][steps_back][confidence_threshold_key]      = 0.0
-                results_dict["results_x_mins_weighted"][steps_back][confidence_threshold_key]   = 0.0
+            ## second scoring method FG_action: replace
+            # calc stake in all accepted bets
+            stake_b = pd.DataFrame()
+            stake_b["delta_price"] = y["delta_price"].apply(lambda y: y - 0.5 * confidence_threshold_adjusted if y > confidence_threshold_adjusted else (y + 0.5 * confidence_threshold_adjusted if -y > confidence_threshold_adjusted else 0))
+            score_correct = (stake_b*x).sum()
+            score_both = abs(stake_b*x).sum()
+            results_dict["results_x_mins_weighted"][steps_back][confidence_threshold_key] = score_correct.values[0] / score_both.values[0]
+        else:
+            results_dict["results_x_mins_PC"][steps_back][confidence_threshold_key]         = 0.0
+            results_dict["results_x_mins_score"][steps_back][confidence_threshold_key]      = 0.0
+            results_dict["results_x_mins_weighted"][steps_back][confidence_threshold_key]   = 0.0
     
     return results_dict
 
