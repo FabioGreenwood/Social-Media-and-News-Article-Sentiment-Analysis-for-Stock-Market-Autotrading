04f4ac6879819d3790bd0911cbb586323d564198
Nick Greenwood <fabio.greenwood@gmail.com>
2024-02-27 20:21:36 +0000

    slight improvements to design

--- Experimental/find_instances_of_design_in_previous_runs.py
+++ Experimental/find_instances_of_design_in_previous_runs.py
@@ -44,27 +44,29 @@ design_space_dict = {
 }
 
 list_of_design_inputs = [
-    [25, 13,  7200,  0, 4, 0, 0, 5, 1, 0.0001, 10, 7],
-    [13, 3,   7200,  0, 2, 0, 0, 4, 0, 1e-11,  8,  9],
-    [13, 7,   180,   1, 1, 1, 0, 4, 3, 1e-09,  25, 0],
-    [25, 13,  7200,  0, 1, 1, 1, 2, 1, 1e-06,  10, 5],
-    [13, 2,   900,   1, 4, 0, 0, 0, 0, 1e-10,  8,  12],
-    [9,  13,  900,   0, 1, 1, 2, 5, 0, 1e-08,  8,  0],
-    [17, 3,   25200, 1, 1, 1, 1, 4, 0, 0.0001, 15, 12],
-    [25, 7,   25200, 0, 4, 1, 2, 2, 3, 1e-09,  15, 9],
-    [17, 7,   7200,  0, 4, 1, 2, 3, 1, 1e-05,  8,  0],
-    [5,  5,   25200, 1, 4, 1, 0, 3, 3, 1e-10,  15, 7],
-    [17, 0.7, 900,   1, 2, 1, 1, 2, 3, 1e-07,  50, 5],
-    [9,  2,   7200,  1, 4, 1, 0, 4, 0, 1e-11,  15, 5],
-    [17, 7,   900,   1, 2, 1, 1, 5, 2, 1e-06, 20, 5],
-    [17, 13,  900,   0, 4, 0, 1, 4, 1, 1e-07,  50, 9],
-    [5,  0.3, 25200, 1, 4, 1, 2, 5, 2, 1e-09,  8,  0],
-    [5,  13,  900,   0, 1, 1, 1, 4, 0, 1e-05,  20, 0],
-    [17, 2,   7200,  0, 2, 1, 2, 0, 2, 0.0001, 8,  7],
-    [13, 13,  900,   1, 2, 0, 0, 3, 0, 1e-05,  50, 9],
-    [13, 13,  900,   0, 1, 1, 1, 1, 0, 1e-05,  10, 5],
-    [25, 0.7, 900,   0, 2, 1, 2, 5, 1, 1e-05,  25, 7],
-    [5,  0.3, 900,   1, 4, 0, 0, 3, 0, 1e-08,  8,  9]
+    [25, 13,  7200,  0, 4, 0, 0, 5, 1, 0.0001, 10, 7],  # 0
+    [13, 3,   7200,  0, 2, 0, 0, 4, 0, 1e-11,  8,  9],  # 1
+    [13, 7,   180,   1, 1, 1, 0, 4, 3, 1e-09,  25, 0],  # 2
+    [25, 13,  7200,  0, 1, 1, 1, 2, 1, 1e-06,  10, 5],  # 3
+    [13, 2,   900,   1, 4, 0, 0, 0, 0, 1e-10,  8,  12], # 4
+    [9,  13,  900,   0, 1, 1, 2, 5, 0, 1e-08,  8,  0],  # 5
+    [17, 3,   25200, 1, 1, 1, 1, 4, 0, 0.0001, 15, 12], # 6
+    [25, 7,   25200, 0, 4, 1, 2, 2, 3, 1e-09,  15, 9],  # 7
+    [17, 7,   7200,  0, 4, 1, 2, 3, 1, 1e-05,  8,  0],  # 8
+    [5,  5,   25200, 1, 4, 1, 0, 3, 3, 1e-10,  15, 7],  # 9
+    [17, 0.7, 900,   1, 2, 1, 1, 2, 3, 1e-07,  50, 5],  # 10
+    [9,  2,   7200,  1, 4, 1, 0, 4, 0, 1e-11,  15, 5],  # 11
+    
+    [17, 7,   900,   1, 2, 1, 1, 5, 2, 1e-06, 20, 5],   # 12
+    
+    [17, 13,  900,   0, 4, 0, 1, 4, 1, 1e-07,  50, 9],  # 13
+    [5,  0.3, 25200, 1, 4, 1, 2, 5, 2, 1e-09,  8,  0],  # 14
+    [5,  13,  900,   0, 1, 1, 1, 4, 0, 1e-05,  20, 0],  # 15
+    [17, 2,   7200,  0, 2, 1, 2, 0, 2, 0.0001, 8,  7],  # 16
+    [13, 13,  900,   1, 2, 0, 0, 3, 0, 1e-05,  50, 9],  # 17
+    [13, 13,  900,   0, 1, 1, 1, 1, 0, 1e-05,  10, 5],  # 18
+    [25, 0.7, 900,   0, 2, 1, 2, 5, 1, 1e-05,  25, 7],  # 19
+    [5,  0.3, 900,   1, 4, 0, 0, 3, 0, 1e-08,  8,  9]   # 20
 ]
 
 
@@ -84,7 +86,7 @@ def compare_design_dict_to_df(design_params_list, df, design_space_dict=design_s
             correct_rows = (df[design.index] == design).all(axis=1)
         elif mode == "implict":
             matching_values = (df[design.index] == design).sum(axis=1)
-            correct_rows    = (matching_values - len(df[design.index].columns) > -3)
+            correct_rows    = (matching_values - len(df[design.index].columns) > -4)
         else:
             raise ValueError("mode setting not recognized")
         if len(df[correct_rows]) > 0:
@@ -97,7 +99,8 @@ def compare_design_dict_to_df(design_params_list, df, design_space_dict=design_s
 
 
 def read_csvs_in_folder(folder_path, file_suffix=".csv"):
-    csv_files = [file for file in os.listdir(folder_path) if file.endswith(file_suffix) and not file.endswith("T" + file_suffix)]
+    csv_files = [file for file in os.listdir(folder_path) if file.endswith(file_suffix) and not file.endswith("T" + file_suffix) and os.path.isfile(os.path.join(folder_path, file))]
+    
     dataframes = {}
     for file in csv_files:
         file_path = os.path.join(folder_path, file)
@@ -118,6 +121,7 @@ def return_keys_within_2_level_dict(input_dict):
 # collect dfs
 
 folder_path = 'C:/Users/Public/fabio_uni_work/Social-Media-and-News-Article-Sentiment-Analysis-for-Stock-Market-Autotrading/precalculated_assets/experiment_records'
+folder_path = 'C:/Users/Public/fabio_uni_work/Social-Media-and-News-Article-Sentiment-Analysis-for-Stock-Market-Autotrading/outputs'
 file_suffix = '.csv'
 list_dataframes = read_csvs_in_folder(folder_path, file_suffix)
 
@@ -126,7 +130,9 @@ for design_ID in range(len(list_of_design_inputs)):
     count_explict = 0
     count_implicit = 0
     history = []
-    for df in list_dataframes.values():
-        count_explict  += compare_design_dict_to_df(list_of_design_inputs[design_ID], df, mode = "explict")
-        count_implicit += compare_design_dict_to_df(list_of_design_inputs[design_ID], df, mode = "implict")
-    print("ID:{} explict:{} implicit:{}".format(design_ID, count_explict, count_implicit))
+    for df in list_dataframes:
+        count_explict  += compare_design_dict_to_df(list_of_design_inputs[design_ID], list_dataframes[df], mode = "explict")
+        count_implicit += compare_design_dict_to_df(list_of_design_inputs[design_ID], list_dataframes[df], mode = "implict")
+        if compare_design_dict_to_df(list_of_design_inputs[design_ID], list_dataframes[df], mode = "implict")>0:
+            history += [df]
+    print("ID:{} explict:{} implicit:{} history:{}".format(design_ID, count_explict, count_implicit, str(history)))
